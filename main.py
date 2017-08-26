d11dfb9d2db428ae612eb8d93ea9ee8b51b239ad
Gan Quan <coin2028@hotmail.com>
2017-08-26 00:00:20 -0700

    try to migrate to github

--- main.py
+++ main.py
@@ -6,15 +6,23 @@ import sys
 import numpy as NP
 import pickle
 
+imgs = []
 with open('imagenet_labels2') as f:
     classes = pickle.load(f)
-x = SPM.imread(sys.argv[1], mode='RGB')
-x = SPM.imresize(x, (224, 224))
-x = NP.expand_dims(x, 0)
 
-x = x.transpose([0, 3, 1, 2]) / 255.
+for filename in sys.argv[1:]:
+    x = SPM.imread(filename, mode='RGB')
+    x = SPM.imresize(x, (224, 224))
+    x = NP.expand_dims(x, 0)
+
+    x = x.transpose([0, 3, 1, 2]) / 255.
+    imgs.append(x)
+x = NP.concatenate(imgs)
 norm = TV.transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+#x = T.autograd.Variable(T.stack([norm(T.FloatTensor(_)) for _ in x]))
 x = T.autograd.Variable(norm(T.FloatTensor(x)))
 vgg19 = TV.models.vgg19(pretrained=True)
+vgg19.eval()
 y = vgg19(x).data.numpy()
-print classes[y.argmax()]
+for idx in y.argmax(axis=1):
+    print classes[idx]
